3d1ddad0d1b798921dda127d1b8d85e0edff2119
Yubo Jing <jybjybjing@YubodeMacBook-Air.local>
2022-10-28 17:15:53 -0400

    Finish lecture 3

--- p3/commerce/auctions/views.py
+++ p3/commerce/auctions/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import User
 
